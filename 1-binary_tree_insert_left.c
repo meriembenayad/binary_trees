c5315d0adfb3f5cc254795adf7ffd17a43c93872
meriembenayad <meriem.benayad@gmail.com>
2023-10-31 14:46:35 +0100

    Task 1: Insert left (FIX)

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * *binary_tree_insert_left - Inserts a node as the left-child of another node
+ * binary_tree_insert_left - Inserts a node as the left-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  *
@@ -8,7 +8,7 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node;
 
 	if (parent == NULL || *parent == NULL)
 		return (NULL);
