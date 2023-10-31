ee8a4ebdf1d402545e293b60cfe9f29896d2835c
meriembenayad <meriem.benayad@gmail.com>
2023-10-31 18:20:58 +0100

    Task 16 - UPDATED

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-size_t binary_tree_height(const binary_tree_t *tree);
-
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: pointer to the root node
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,8 +43,13 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	right_height = (int)binary_tree_height(tree->right);
 
 	if (left_height == right_height)
-		return (binary_tree_is_perfect(tree->left)
-				&& binary_tree_is_perfect(tree->right));
+	{
+		if (left_height == 0 && right_height == 0)
+			return (1);
+
+		return (binary_tree_is_perfect(tree->left) &&
+					binary_tree_is_perfect(tree->right));
+	}
 
 	return (0);
 }
