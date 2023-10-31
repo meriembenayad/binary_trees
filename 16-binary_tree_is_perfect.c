b0b7267437248a387441ed519a81ec9dd0f268c1
meriembenayad <meriem.benayad@gmail.com>
2023-10-31 17:55:14 +0100

    Task 16 - UPDATED

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,10 +14,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		height_left = 1 + binary_tree_height(tree->left);
+		height_left = binary_tree_height(tree->left);
 
 	if (tree->right)
-		height_right = 1 + binary_tree_height(tree->right);
+		height_right = binary_tree_height(tree->right);
 
 	if (height_left > height_right)
 		return (height_left);
