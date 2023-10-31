34fcbd731a245b0d8c0284327867df4730541839
meriembenayad <meriem.benayad@gmail.com>
2023-10-31 16:57:14 +0100

    Task 1: Insert left (FIX)

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,19 +10,20 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent == NULL)
+	if (!parent)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
 
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->left)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
+
 	parent->left = new_node;
 
 	return (new_node);
