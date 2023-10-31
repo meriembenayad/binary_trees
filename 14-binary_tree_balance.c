1b21db2f93eee35153ac96369fbe8b27c21dffb8
meriembenayad <meriem.benayad@gmail.com>
2023-10-31 18:36:41 +0100

    Task 14 - UPDATED

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -42,8 +42,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 
-	if (height_left < height_right)
-		return (height_right + 1);
-
-	return (height_left + 1);
+	return (height_left - height_right);
 }
