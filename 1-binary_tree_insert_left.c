6c03ae144fd461d32bc9bd1af65159a870f343cc
meriembenayad <meriem.benayad@gmail.com>
2023-10-31 14:55:18 +0100

    Task 1: Insert left (RE FAILED)

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,12 +14,13 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = binary_tree_node(parent, value);
+
 	if (new_node == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
 	{
-		/* new_node->left = parent->left; */
+		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
